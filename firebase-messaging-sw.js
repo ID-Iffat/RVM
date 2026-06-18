fbb8b405792ae9463f512705a97962a31905a9a6
ID-Iffat <154099890+ID-Iffat@users.noreply.github.com>
2026-06-18 16:44:17 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -10,21 +10,21 @@ firebase.initializeApp({
   appId: "1:814557927417:web:720a45e7abaebb1207cf28"
 });
 
-// const messaging = firebase.messaging();
-// messaging.onBackgroundMessage((payload) => {
-//   console.log("[firebase-messaging-sw.js] Background message received", payload);
-// });
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
   console.log("[firebase-messaging-sw.js] Background message received", payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/RVM/icon-192x192.png', 
-    badge: '/RVM/icon-192x192.png',
-    data: {
-      click_action: payload.notification.click_action || "https://id-iffat.github.io/RVM/"
-    }
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
 });
+// const messaging = firebase.messaging();
+// messaging.onBackgroundMessage((payload) => {
+//   console.log("[firebase-messaging-sw.js] Background message received", payload);
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//     icon: '/RVM/icon-192x192.png', 
+//     badge: '/RVM/icon-192x192.png',
+//     data: {
+//       click_action: payload.notification.click_action || "https://id-iffat.github.io/RVM/"
+//     }
+//   };
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
